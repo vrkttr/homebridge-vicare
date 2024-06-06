e7a460ed5ee7e0ed250b0bfd922bd210d684f160
David Vierkötter <github@vrkttr.de>
2024-06-06 14:48:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,288 +1,220 @@
-const request = require('request');
+'use strict';
 
-let Service, Characteristic;
+const request = require('request');
+let Service, Characteristic, Accessory, UUIDGen;
 
 module.exports = (homebridge) => {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-vicare', 'ViCareThermostat', ViCareThermostat);
+  Accessory = homebridge.platformAccessory;
+  UUIDGen = homebridge.hap.uuid;
+  homebridge.registerPlatform('homebridge-vicare', 'ViCareThermostatPlatform', ViCareThermostatPlatform, true);
 };
 
-class ViCareThermostat {
+class ViCareThermostatPlatform {
   constructor(log, config, api) {
     this.log = log;
     this.config = config;
     this.api = api;
-
-    this.name = config.name;
-    this.apiEndpoint = config.apiEndpoint || 'https://api.viessmann.com/iot/v2';
     this.accessToken = config.accessToken;
-    this.debug = config.debug || false;
+    this.apiEndpoint = config.apiEndpoint;
+    this.devices = config.devices;
+    this.accessories = [];
 
-    this.services = [];
-
-    this.setupInformationService();
-    this.setupThermostatService();
-
-    this.retrieveIds().then(() => {
-      this.log('Alle notwendigen IDs abgerufen.');
-      this.listAvailableFeatures();
-    }).catch(err => {
-      this.log('Fehler beim Abrufen der IDs:', err);
+    this.api.on('didFinishLaunching', () => {
+      this.retrieveIds((err, installationId, gatewaySerial) => {
+        if (!err) {
+          this.installationId = installationId;
+          this.gatewaySerial = gatewaySerial;
+          this.devices.forEach(deviceConfig => {
+            this.addAccessory(deviceConfig);
+          });
+        } else {
+          this.log('Error retrieving installation or gateway IDs:', err);
+        }
+      });
     });
   }
 
-  setupInformationService() {
-    this.informationService = new Service.AccessoryInformation();
-    this.informationService
+  configureAccessory(accessory) {
+    this.accessories.push(accessory);
+  }
+
+  retrieveIds(callback) {
+    const options = {
+      url: `${this.apiEndpoint}/equipment/installations`,
+      headers: {
+        'Authorization': `Bearer ${this.accessToken}`,
+      },
+      json: true
+    };
+
+    request.get(options, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        this.log('Error retrieving installations:', error || body);
+        callback(error || new Error(body));
+        return;
+      }
+
+      this.log('Installation data:', body);
+      const installation = body.data[0];
+      const installationId = installation.id;
+
+      const gatewayOptions = {
+        url: `${this.apiEndpoint}/equipment/installations/${installationId}/gateways`,
+        headers: {
+          'Authorization': `Bearer ${this.accessToken}`,
+        },
+        json: true
+      };
+
+      request.get(gatewayOptions, (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          this.log('Error retrieving gateways:', error || body);
+          callback(error || new Error(body));
+          return;
+        }
+
+        this.log('Gateway data:', body);
+        if (!body.data || body.data.length === 0) {
+          this.log('No gateway data available.');
+          callback(new Error('No gateway data available.'));
+          return;
+        }
+
+        const gateway = body.data[0];
+        const gatewaySerial = gateway.serial;
+        callback(null, installationId, gatewaySerial);
+      });
+    });
+  }
+
+  addAccessory(deviceConfig) {
+    const uuid = UUIDGen.generate(deviceConfig.name);
+    let accessory = this.accessories.find(acc => acc.UUID === uuid);
+
+    if (!accessory) {
+      accessory = new Accessory(deviceConfig.name, uuid);
+      this.api.registerPlatformAccessories('homebridge-vicare', 'ViCareThermostatPlatform', [accessory]);
+      this.accessories.push(accessory);
+    }
+
+    const vicareAccessory = new ViCareThermostatAccessory(
+      this.log,
+      deviceConfig,
+      this.api,
+      this.accessToken,
+      this.apiEndpoint,
+      this.installationId,
+      this.gatewaySerial
+    );
+
+    accessory.context.deviceConfig = deviceConfig;
+    accessory.getService(Service.AccessoryInformation)
       .setCharacteristic(Characteristic.Manufacturer, 'Viessmann')
       .setCharacteristic(Characteristic.Model, 'ViCare')
-      .setCharacteristic(Characteristic.SerialNumber, '123-456-789');
+      .setCharacteristic(Characteristic.SerialNumber, 'Default-Serial');
 
-    this.services.push(this.informationService);
+    vicareAccessory.getServices().forEach(service => {
+      const existingService = accessory.getServiceById(service.UUID, service.subtype);
+      if (existingService) {
+        accessory.removeService(existingService);
+      }
+      accessory.addService(service);
+    });
+
+    this.api.updatePlatformAccessories([accessory]);
   }
+}
 
-  setupThermostatService() {
-    this.thermostatService = new Service.Thermostat(this.name);
+class ViCareThermostatAccessory {
+  constructor(log, config, api, accessToken, apiEndpoint, installationId, gatewaySerial) {
+    this.log = log;
+    this.name = config.name;
+    this.feature = config.feature;
+    this.apiEndpoint = apiEndpoint;
+    this.accessToken = accessToken;
+    this.deviceId = config.deviceId;
+    this.installationId = installationId;
+    this.gatewaySerial = gatewaySerial;
 
-    this.thermostatService
+    this.temperatureService = new Service.TemperatureSensor(this.name, `temperatureService_${this.name}_${this.feature}_${UUIDGen.generate(this.name + this.feature)}`);
+    this.temperatureService
       .getCharacteristic(Characteristic.CurrentTemperature)
-      .on('get', this.getCurrentTemperature.bind(this));
+      .on('get', this.getTemperature.bind(this));
 
-    this.thermostatService
-      .getCharacteristic(Characteristic.TargetTemperature)
-      .on('get', this.getTargetTemperature.bind(this))
-      .on('set', this.setTargetTemperature.bind(this))
-      .setProps({
-        minValue: 10,
-        maxValue: 82,
-        minStep: 1
-      });
+    if (config.feature.includes('burners')) {
+      this.switchService = new Service.Switch(this.name, `switchService_${this.name}_${this.feature}_${UUIDGen.generate(this.name + this.feature)}`);
+      this.switchService
+        .getCharacteristic(Characteristic.On)
+        .on('get', this.getBurnerStatus.bind(this))
+        .on('set', this.setBurnerStatus.bind(this));
+    }
 
-    this.services.push(this.thermostatService);
+    this.services = [this.temperatureService];
+    if (this.switchService) {
+      this.services.push(this.switchService);
+    }
   }
 
-  async retrieveIds() {
-    this.installationId = await this.getInstallationId();
-    this.gatewaySerial = await this.getGatewaySerial();
-    this.deviceId = await this.getDeviceId();
-  }
-
-  getInstallationId() {
-    return new Promise((resolve, reject) => {
-      request.get({
-        url: `${this.apiEndpoint}/equipment/installations`,
-        headers: { 'Authorization': `Bearer ${this.accessToken}` }
-      }, (error, response, body) => {
-        if (error) return reject(error);
-        const data = JSON.parse(body);
-        if (this.debug) this.log('Installationsdaten:', data);
-        if (data.data && data.data.length > 0) {
-          resolve(data.data[0].id);
-        } else {
-          reject(new Error('Keine Installationen gefunden'));
-        }
-      });
-    });
-  }
-
-  getGatewaySerial() {
-    return new Promise((resolve, reject) => {
-      request.get({
-        url: `${this.apiEndpoint}/equipment/gateways`,
-        headers: { 'Authorization': `Bearer ${this.accessToken}` }
-      }, (error, response, body) => {
-        if (error) return reject(error);
-        const data = JSON.parse(body);
-        if (this.debug) this.log('Gateway-Daten:', data);
-        if (data.data && data.data.length > 0) {
-          resolve(data.data[0].serial);
-        } else {
-          reject(new Error('Keine Gateways gefunden'));
-        }
-      });
-    });
-  }
-
-  getDeviceId() {
-    return new Promise((resolve, reject) => {
-      const url = `${this.apiEndpoint}/equipment/installations/${this.installationId}/gateways/${this.gatewaySerial}/devices`;
-      request.get({
+  getTemperature(callback) {
+    const url = `${this.apiEndpoint}/features/installations/${this.installationId}/gateways/${this.gatewaySerial}/devices/${this.deviceId}/features/${this.feature}`;
+    request.get(
+      {
         url: url,
-        headers: { 'Authorization': `Bearer ${this.accessToken}` }
-      }, (error, response, body) => {
-        if (error) return reject(error);
-        const data = JSON.parse(body);
-        if (this.debug) this.log('Gerätedaten:', data);
-        if (data.data && data.data.length > 0) {
-          resolve(data.data[0].id);
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+        json: true,
+      },
+      (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const data = body.data || body;
+          if (data.properties && data.properties.value && data.properties.value.value !== undefined) {
+            const temp = data.properties.value.value;
+            callback(null, temp);
+          } else {
+            this.log('Unexpected response structure:', data);
+            callback(new Error('Unexpected response structure.'));
+          }
         } else {
-          reject(new Error('Keine Geräte gefunden'));
-        }
-      });
-    });
-  }
-
-  listAvailableFeatures() {
-    const url = `${this.apiEndpoint}/features/installations/${this.installationId}/gateways/${this.gatewaySerial}/devices/${this.deviceId}/features`;
-    request.get({
-      url: url,
-      headers: { 'Authorization': `Bearer ${this.accessToken}` }
-    }, (error, response, body) => {
-      if (error) {
-        this.log('Fehler beim Auflisten der verfügbaren Funktionen:', error);
-      } else {
-        const data = JSON.parse(body).data;
-        if (this.debug) this.log('Verfügbare Funktionen:', data);
-        if (Array.isArray(data)) {
-          this.createSensorsAndSwitches(data);
-        } else {
-          this.log('Unerwartetes Datenformat für Funktionen:', data);
+          this.log('Error fetching temperature:', error || body);
+          callback(error || new Error(body));
         }
       }
-    });
+    );
   }
 
-  createSensorsAndSwitches(features) {
-    features.forEach(feature => {
-      const { feature: featureName, properties } = feature;
-
-      if (properties && properties.value && typeof properties.value.value === 'number') {
-        const sensorService = new Service.TemperatureSensor(featureName);
-
-        sensorService
-          .getCharacteristic(Characteristic.CurrentTemperature)
-          .on('get', (callback) => {
-            callback(null, properties.value.value);
-          });
-
-        this.services.push(sensorService);
-        this.log(`Sensor hinzugefügt: ${featureName}`);
-      }
-
-      if (featureName === "heating.burners.0" && properties.active) {
-        const switchService = new Service.Switch(featureName);
-
-        switchService
-          .getCharacteristic(Characteristic.On)
-          .on('get', (callback) => {
-            callback(null, properties.active.value);
-          });
-
-        this.services.push(switchService);
-        this.log(`Schalter hinzugefügt: ${featureName}`);
-      }
-
-      // Raumtemperatursensor (Heizkreis 1)
-      if (featureName === 'heating.circuits.0.sensors.temperature.room' && properties.value) {
-        const roomTemperatureSensor = new Service.TemperatureSensor('Raumtemperatursensor');
-
-        roomTemperatureSensor
-          .getCharacteristic(Characteristic.CurrentTemperature)
-          .on('get', (callback) => {
-            callback(null, properties.value.value);
-          });
-
-        this.services.push(roomTemperatureSensor);
-        this.log('Sensor hinzugefügt: Raumtemperatursensor');
-      }
-
-      // Versorgungstemperatursensor
-      if (featureName === 'heating.circuits.0.sensors.temperature.supply' && properties.value) {
-        const supplyTemperatureSensor = new Service.TemperatureSensor('Versorgungstemperatursensor');
-
-        supplyTemperatureSensor
-          .getCharacteristic(Characteristic.CurrentTemperature)
-          .on('get', (callback) => {
-            callback(null, properties.value.value);
-          });
-
-        this.services.push(supplyTemperatureSensor);
-        this.log('Sensor hinzugefügt: Versorgungstemperatursensor');
-      }
-
-      // Warmwasserspeichertemperatursensor
-      if (featureName === 'heating.dhw.sensors.temperature.dhwCylinder' && properties.value) {
-        const hotWaterTemperatureSensor = new Service.TemperatureSensor('Warmwasserspeichertemperatursensor');
-
-        hotWaterTemperatureSensor
-          .getCharacteristic(Characteristic.CurrentTemperature)
-          .on('get', (callback) => {
-            callback(null, properties.value.value);
-          });
-
-        this.services.push(hotWaterTemperatureSensor);
-        this.log('Sensor hinzugefügt: Warmwasserspeichertemperatursensor');
-      }
-    });
-
-    this.log(`Hinzugefügte Sensoren und Schalter: ${this.services.length - 1}`);
-  }
-
-  getCurrentTemperature(callback) {
-    if (!this.deviceId) {
-      return callback(new Error('Geräte-ID noch nicht verfügbar'));
-    }
-    const url = `${this.apiEndpoint}/features/installations/${this.installationId}/gateways/${this.gatewaySerial}/devices/${this.deviceId}/features/heating.circuits.0.operating.programs.normal`;
-    request.get({
-      url: url,
-      headers: { 'Authorization': `Bearer ${this.accessToken}` }
-    }, (error, response, body) => {
-      if (error) {
-        callback(error);
-      } else {
-        const data = JSON.parse(body).data;
-        const currentTemperature = parseFloat(data.properties.temperature.value);
-        if (!isNaN(currentTemperature) && isFinite(currentTemperature)) {
-          callback(null, currentTemperature);
+  getBurnerStatus(callback) {
+    const url = `${this.apiEndpoint}/features/installations/${this.installationId}/gateways/${this.gatewaySerial}/devices/${this.deviceId}/features/${this.feature}`;
+    request.get(
+      {
+        url: url,
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+        json: true,
+      },
+      (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const data = body.data || body;
+          if (data.properties && data.properties.active && data.properties.active.value !== undefined) {
+            const isActive = data.properties.active.value;
+            callback(null, isActive);
+          } else {
+            this.log('Unexpected response structure:', data);
+            callback(new Error('Unexpected response structure.'));
+          }
         } else {
-          callback(new Error('Ungültiger Wert für die aktuelle Temperatur'));
+          this.log('Error fetching burner status:', error || body);
+          callback(error || new Error(body));
         }
       }
-    });
+    );
   }
 
-  getTargetTemperature(callback) {
-    if (!this.deviceId) {
-      return callback(new Error('Geräte-ID noch nicht verfügbar'));
-    }
-    const url = `${this.apiEndpoint}/features/installations/${this.installationId}/gateways/${this.gatewaySerial}/devices/${this.deviceId}/features/heating.circuits.0.operating.programs.normal`;
-    request.get({
-      url: url,
-      headers: { 'Authorization': `Bearer ${this.accessToken}` }
-    }, (error, response, body) => {
-      if (error) {
-        callback(error);
-      } else {
-        const data = JSON.parse(body).data;
-        const targetTemperature = parseFloat(data.properties.temperature.value);
-        if (!isNaN(targetTemperature) && isFinite(targetTemperature)) {
-          callback(null, targetTemperature);
-        } else {
-          callback(new Error('Ungültiger Wert für die Zieltemperatur'));
-        }
-      }
-    });
-  }
-
-  setTargetTemperature(value, callback) {
-    if (!this.deviceId) {
-      return callback(new Error('Geräte-ID noch nicht verfügbar'));
-    }
-    const url = `${this.apiEndpoint}/features/installations/${this.installationId}/gateways/${this.gatewaySerial}/devices/${this.deviceId}/features/heating.circuits.0.operating.programs.normal/commands/setTemperature`;
-    request.post({
-      url: url,
-      headers: { 'Authorization': `Bearer ${this.accessToken}` },
-      json: {
-        targetTemperature: value
-      }
-    }, (error, response, body) => {
-      if (error) {
-        callback(error);
-      } else {
-        callback();
-      }
-    });
+  setBurnerStatus(value, callback) {
+    callback(null);
   }
 
   getServices() {
